81adacc79f094dcecc9c9ab6eab8d7db2e71d01b
slayertenwolf <slayerten@163.com>
2021-09-25 16:35:54 +0800

    change window

--- tkinterTest.py
+++ tkinterTest.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 #from tkinter import *
 import tkinter as tk
+from tkinter.messagebox import showinfo
 
+def reply():
+    showinfo(title='Zoom',message='You You You!')
 win = tk.Tk() 
 win.title("HelloWorld")
+btn = tk.Button(win,text='press me!',command=reply)
+btn.pack()
 win.mainloop()

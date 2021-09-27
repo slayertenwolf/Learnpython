6ecc0b8b63ff1bc514fca1a28db65626acad5d0c
slayertenwolf <slayerten@163.com>
2021-09-27 17:03:00 +0800

    add files

---
+++ cgi-bin/hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+print("Content-type:text/html")
+print
+print('<html>')
+print('<head>')
+print('<meta charset="utf-8">')
+print('<title>Hello world-My first CGI !!!!</title>')
+print('</head>')
+print('<body>')
+print("<h2>Hello World!This is my first .</h2>")
+print('</body>')
+print('</html>')
